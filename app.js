601bec88a98b5b845b13a60e4aed3c6a11c75457
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2021-12-19 02:24:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const button=document.querySelector("button");
+const body=document.querySelector("body");
+const color=['red','yellow','green','white','black','pink','purple']
+
+body.style.backgroundColor='violet';
+
+button.addEventListener('click',changeB);
+
+function changeB(){
+    const colorIndex=parseInt(Math.random()*color.length)
+    body.style.backgroundColor=color[colorIndex]
+}
